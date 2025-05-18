80a374f727dedc9b40a131531407bee2f81cebd5
Maverick-Services <maverickit123@gmail.com>
2025-05-18 08:58:27 +0530

    a

--- src/app/admin/blogs/form/layout.jsx
+++
@@ -1,5 +0,0 @@
-import BlogFormContextProvider from "./contexts/BlogFormContext";
-
-export default function Layout({ children }) {
-    return <BlogFormContextProvider>{children}</BlogFormContextProvider>
-}
