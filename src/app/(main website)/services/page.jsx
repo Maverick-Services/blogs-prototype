9d4f225cd29e4641e97a11b3116a65864bafe995
Maverick-Services <maverickit123@gmail.com>
2025-05-28 17:35:58 +0530

    bugs fix

--- src/app/(main website)/services/page.jsx
+++ src/app/(main website)/services/page.jsx
@@ -3,14 +3,15 @@ import ServicesClient from './ServicesClient'    // ← New client wrapper
 
 export default async function Page() {
     const [servicesRes, categoriesRes] = await Promise.all([
-        fetch("http://localhost:3000/api/services", { next: { revalidate: 300 } }),
-        fetch("http://localhost:3000/api/categories", { next: { revalidate: 300 } })
+        fetch("https://blogs-prototype-khaki.vercel.app/api/services", { next: { revalidate: 300 } }),
+        fetch("https://blogs-prototype-khaki.vercel.app/categories", { next: { revalidate: 300 } })
     ])
-    const services = await servicesRes.json()
-    const categories = await categoriesRes.json()
+    // const services = await servicesRes.json()
+    // const categories = await categoriesRes.json()
 
-    console.log(services)
-    console.log(categories)
+    // console.log(services)
+    // console.log(categories)
 
-    return <ServicesClient services={services?.data} categories={categories?.data} />
+    return <ServicesClient s />
+    // return <div>Services</div>
 }
