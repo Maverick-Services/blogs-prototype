9296da4b87a614b825b02f8f3402f589868c7027
Maverick-Services <maverickit123@gmail.com>
2025-07-10 23:56:35 +0530

    update

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import WebsiteLayout from '@/components/website/WebsiteLayout';
-import { getHomePageData } from '@/lib/main/getHomePageData';
+import { getCategories, getServices } from '@/lib/main/getHomePageData';
 import UserMain from './components/UserMain';
 
 export default async function page() {
 
-    const { services, categories } = await getHomePageData();
+    const servicesData = await getServices();
+    const services = servicesData?.data || [];
+    const categoriesData = await getCategories();
+    const categories = categoriesData?.data || [];
 
     return (
         <WebsiteLayout services={services} categories={categories}>
